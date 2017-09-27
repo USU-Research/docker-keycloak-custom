0e4e327e01405ce6adf93a093181400fb2082a17
Alexander Krieg <alexander.krieg@arcor.de>
2017-09-27 15:02:42 +0200

    added docker entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+CUSTOM_XML_FILE=/conf/standalone.xml
+if [ -e $CUSTOM_XML_FILE ]; then
+     cp $CUSTOM_XML_FILE "/opt/jboss/keycloak/standalone/configuration/standalone.xml"
+fi
+cat "/opt/jboss/keycloak/standalone/configuration/standalone.xml"
+
+CUSTOM_THEMES=/opt/jboss/keycloak/customthemes
+
+if [ -d "$CUSTOM_THEMES" ]; then
+    cp -r ${CUSTOM_THEMES}/* /opt/jboss/keycloak/themes
+fi
+
+if [ $EXPORT_FILE ] ; then
+    exec /opt/jboss/keycloak/bin/standalone.sh -Dkeycloak.migration.action=export -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=$EXPORT_FILE
+fi
+
+if [ $IMPORT_FILE ] ; then
+    exec /opt/jboss/keycloak/bin/standalone.sh -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=$IMPORT_FILE
+fi
+
+if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
+    keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+fi
+
+exec /opt/jboss/keycloak/bin/standalone.sh $@
+exit $?
